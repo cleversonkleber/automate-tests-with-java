2a31137d2e2c82965df6581c0d0d7575d1caae4d
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-02-02 02:06:39 -0300

    Criação da função para

---
+++ src/test/java/com/cleverson/automate_tests_with_java/math/ArrayCompareTest.java
@@ -0,0 +1,32 @@
+package com.cleverson.automate_tests_with_java.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.Arrays;
+
+public class ArrayCompareTest {
+    @Test
+    void test(){
+        int[] numbers={25,8,21,32,3};
+        int[] expectedArray={3,8,21,25,32};
+
+        Arrays.sort(numbers);
+        Assertions.assertArrayEquals(numbers, expectedArray);
+    }
+
+    @Test
+    @Timeout(1)
+    void testSortPerformance(){
+        int[] numbers={25,8,21,32,3};
+        int[] expectedArray={3,8,21,25,32};
+        for (int i=0;i<100000;i++){
+            numbers[0] = i;
+            Arrays.sort(numbers);
+        }
+
+        Arrays.sort(numbers);
+        Assertions.assertArrayEquals(numbers, expectedArray);
+    }
+}
