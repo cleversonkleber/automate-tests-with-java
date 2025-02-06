1d4360bd2e13f7a282987838b9371a25cfee3397
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-02-05 23:51:11 -0300

    @Order(3)

--- src/test/java/com/cleverson/automate_tests_with_java/math/MethodOrderdRandonlyTest.java
+++ src/test/java/com/cleverson/automate_tests_with_java/math/MethodOrderdRandonlyTest.java
@@ -1,6 +1,7 @@
 package com.cleverson.automate_tests_with_java.math;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -8,15 +9,18 @@ import org.junit.jupiter.api.TestMethodOrder;
 public class MethodOrderdRandonlyTest {
 
     @Test
+    @Order(1)
     void testA(){
         System.out.println("Running Test A");
     }
     @Test
+    @Order(2)
     void testB(){
         System.out.println("Running Test B");
     }
 
     @Test
+    @Order(3)
     void testC(){
         System.out.println("Running Test C");
     }
